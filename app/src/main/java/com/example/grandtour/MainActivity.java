a8950e09c03bf62bd65e80a1060ddd0e2c48ed8e
SteTrotto <s.trotto@campus.unimib.it>
2021-11-20 22:25:34 +0100

    test

--- app/src/main/java/com/example/grandtour/MainActivity.java
+++ app/src/main/java/com/example/grandtour/MainActivity.java
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
-    //gg
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
