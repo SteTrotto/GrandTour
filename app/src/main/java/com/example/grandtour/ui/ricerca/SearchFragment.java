4541b9d793d54576242d270dcb45d7eb7fea0d7b
SteTrotto <s.trotto@campus.unimib.it>
2021-11-20 23:43:19 +0100

    search fragment start

--- app/src/main/java/com/example/grandtour/ui/ricerca/SearchFragment.java
+++ app/src/main/java/com/example/grandtour/ui/ricerca/SearchFragment.java
@@ -1,26 +1,40 @@
 package com.example.grandtour.ui.ricerca;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.grandtour.R;
 import com.example.grandtour.databinding.FragmentSearchBinding;
+
 
 public class SearchFragment extends Fragment {
 
     private SearchViewModel searchViewModel;
     private FragmentSearchBinding binding;
 
+    private ImageButton imageButton;
+    private EditText editText;
+    private EditText editDate;
+    private EditText editTrasporto;
+
+    final private String TAG_S = "SEARCH";
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         searchViewModel =
                 new ViewModelProvider(this).get(SearchViewModel.class);
 
@@ -34,6 +48,30 @@ public class SearchFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+
+        imageButton = root.findViewById(R.id.search_button);
+        editText = root.findViewById(R.id.search_regione);
+        editDate = root.findViewById(R.id.search_data);
+        editTrasporto = root.findViewById(R.id.search_trasporto);
+
+        //test bottone
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v) {
+                //String.valueOf(editText.getText()) prende il valore in stringa del editText
+                Log.d(TAG_S, String.valueOf(editText.getText()));
+                Log.d(TAG_S, String.valueOf(editDate.getText()));
+                Log.d(TAG_S, String.valueOf(editTrasporto.getText()));
+            }
+        }
+        );
+
+
+
+
+
         return root;
     }
 
