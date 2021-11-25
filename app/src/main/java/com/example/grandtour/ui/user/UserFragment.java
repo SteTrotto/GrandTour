58541eb7ed1d382133b901734c011d0311bd8911
Gabriele <g.anteghini@campus.unimi.it>
2021-11-25 09:55:39 +0100

    Controlli user

--- app/src/main/java/com/example/grandtour/ui/user/UserFragment.java
+++ app/src/main/java/com/example/grandtour/ui/user/UserFragment.java
@@ -1,9 +1,13 @@
 package com.example.grandtour.ui.user;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,12 +16,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.grandtour.R;
 import com.example.grandtour.databinding.FragmentUserBinding;
 
 public class UserFragment extends Fragment {
 
     private UserViewModel UserViewModel;
     private FragmentUserBinding binding;
+    private EditText Edit_Nome_User;
+    private EditText Edit_Pwd_User;
+    private Button Button_User;
+
+    final private String TAG_S = "USER";
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,6 +45,38 @@ public class UserFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        //associa i valori alle variabili
+        Edit_Nome_User = root.findViewById(R.id.login_name);
+        Edit_Pwd_User = root.findViewById(R.id.login_password);
+        Button_User = root.findViewById(R.id.login);
+
+        //bottone sing in
+        Button_User.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //String.valueOf(editText.getText()) prende il valore in stringa del editText
+                Log.d(TAG_S, String.valueOf(Edit_Nome_User.getText()));
+                Log.d(TAG_S, String.valueOf(Edit_Pwd_User.getText()));
+                //controllo log-in
+                if(String.valueOf(Edit_Nome_User.getText()).equals("tina") && String.valueOf(Edit_Pwd_User.getText()).equals("tina"))
+                    System.out.println("succeso");
+                else
+                    {
+                        System.out.println("errore");
+                }
+
+
+            }
+        });
+
+
+
+
+
+
+
         return root;
     }
 
