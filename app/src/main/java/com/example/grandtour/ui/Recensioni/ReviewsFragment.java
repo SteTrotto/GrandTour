9fb4c8aa63f1ab794fc0b1693d81772b0108edbd
cfortezza <c.fortezza@campus.unimib.it>
2021-11-25 11:37:19 +0100

    Carmine 25/11/2021

--- app/src/main/java/com/example/grandtour/ui/Recensioni/ReviewsFragment.java
+++ app/src/main/java/com/example/grandtour/ui/Recensioni/ReviewsFragment.java
@@ -1,5 +1,6 @@
 package com.example.grandtour.ui.Recensioni;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,16 +11,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.grandtour.R;
 import com.example.grandtour.databinding.FragmentReviewsBinding;
+import com.example.grandtour.ui.ricerca_recensione.Ricerca_RecensioneFragment;
 
 
 public class ReviewsFragment extends Fragment {
 
     private ReviewsViewModel reviewsViewModel;
     private FragmentReviewsBinding binding;
+
+    private Button bottone;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,14 +35,27 @@ public class ReviewsFragment extends Fragment {
         binding = FragmentReviewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final Button button = binding.button;
-       /* reviewsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //final Button button = binding.button;
+
+
+        bottone = root.findViewById(R.id.button);
+        bottone.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Fragment fragment = null;
+                fragment =new Ricerca_RecensioneFragment();
+                replaceFragment(fragment);
+
             }
-        });*/
+        });
+
         return root;
+    }
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.nav_host_fragment_activity_main, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 
@@ -45,6 +64,7 @@ public class ReviewsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 
 }
 
--- app/src/main/java/com/example/grandtour/ui/ricerca_recensione/Ricerca_RecensioneFragment.java
+++ app/src/main/java/com/example/grandtour/ui/ricerca_recensione/Ricerca_RecensioneFragment.java
@@ -1,23 +1,29 @@
 package com.example.grandtour.ui.ricerca_recensione;
-
+import com.example.grandtour.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.grandtour.databinding.FragmentReviewsBinding;
 import com.example.grandtour.databinding.FragmentRicercaRecensioneBinding;
-import com.example.grandtour.ui.Recensioni.ReviewsViewModel;
 
-public class Ricerca_RecensioneFragment extends AppCompatActivity {
+import java.util.ArrayList;
+
+
+public class Ricerca_RecensioneFragment extends Fragment/*AppCompatActivity*/ {
     private Ricerca_RecensioneViewModel ricerca_recViewModel;
     private FragmentRicercaRecensioneBinding binding;
 
@@ -28,13 +34,66 @@ public class Ricerca_RecensioneFragment extends AppCompatActivity {
         binding = FragmentRicercaRecensioneBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.b;
-        ricerca_recViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
+
+    //-------------------------------------------------------------------
+   /* ListView listView;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        View rootView = inflater.inflate(R.layout.about_screeen, container, false);
+        return rootView;
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Pineapple");
+        list.add("Orange");
+        list.add("Lychee");
+        list.add("Gavava");
+        list.add("Peech");
+        list.add("Melon");
+        list.add("Watermelon");
+        list.add("Papaya");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        MenuItem searchViewItem = menu.findItem(R.id.app_bar_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();*/
+             /*   if(list.contains(query)){
+                    adapter.getFilter().filter(query);
+                }else{
+                    Toast.makeText(MainActivity.this, "No Match found",Toast.LENGTH_LONG).show();
+                }*/
+          /*      return false;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+}*/
 }
