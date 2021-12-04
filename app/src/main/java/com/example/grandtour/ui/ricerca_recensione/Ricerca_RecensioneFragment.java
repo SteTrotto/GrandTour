f7305929f935e7a7b205064d02de588b1daab7cc
cfortezza <c.fortezza@campus.unimib.it>
2021-12-04 19:50:48 +0100

    Carmine completamento layout recensioni

--- app/src/main/java/com/example/grandtour/ui/ricerca_recensione/Ricerca_RecensioneFragment.java
+++ app/src/main/java/com/example/grandtour/ui/ricerca_recensione/Ricerca_RecensioneFragment.java
@@ -2,15 +2,18 @@ package com.example.grandtour.ui.ricerca_recensione;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,26 +30,202 @@ import java.util.ArrayList;
 
 
 
-  public class Ricerca_RecensioneFragment extends Fragment/*AppCompatActivity*/ {
-
-
-
+  public class Ricerca_RecensioneFragment extends Fragment /*AppCompatActivity*/ {
 
     private Ricerca_RecensioneViewModel ricerca_recViewModel;
     private FragmentRicercaRecensioneBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+
+
+
+
+      public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ricerca_recViewModel = new ViewModelProvider(this).get(Ricerca_RecensioneViewModel.class);
 
         binding = FragmentRicercaRecensioneBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        return root;
+
+
+        //riempiamo lo spinner
+        View v = inflater.inflate(R.layout.fragment_ricerca_recensione,container,false);
+        Spinner a = (Spinner) v.findViewById(R.id.spinner_search_regione);
+
+        Spinner b = (Spinner) v.findViewById(R.id.spinner_search_viaggio);
+       /* ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
+                R.array.regione, android.R.layout.simple_spinner_item);
+
+        b.setAdapter(adapter);
+        */
+
+        a.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                /*
+                Log.d("myTag", "This is my message");
+               if(pos == 1) { //se abruzzo è selezionato
+                   Log.d("myTag", "Sei in abruzzo");
+                }*/
+                ArrayAdapter<CharSequence> adapter;
+                switch(pos) {
+
+                    case 1:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiAbruzzo,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 2:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiBasilicata,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 3:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiCalabria,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 4:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiCampania,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 5:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiEmilia,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 6:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiFriuli,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 7:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiLazio,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 8:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiLiguria,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 9:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiLombardia,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 10:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiMarche,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 11:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiMolise,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 12:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiPiemonte,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 13:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiPuglia,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 14:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiSardegna,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 15:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiSicilia,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 16:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiToscana,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 17:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiTrentino,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 18:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiUmbria,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 19:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiAosta,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                    case 20:
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.ViaggiVeneto,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+
+
+                    default:
+                        //contiene anche il case 0
+                        adapter = ArrayAdapter
+                                .createFromResource(view.getContext(), R.array.Iniziale,
+                                        android.R.layout.simple_spinner_dropdown_item);
+                        b.setAdapter(adapter);
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+            });
+
+        return v; //or return root
+
     }
 
-    //-------------------------------------------------------------------
+  }
 
 
-}
+                    /*ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_item,
+                            getResources().getStringArray(R.array.viaggiAbruzzo));
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    */
+
+
+       // adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+
+
+
+
+
 
