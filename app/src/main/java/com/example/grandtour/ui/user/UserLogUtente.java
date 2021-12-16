e2bc63a12f7c5f4278210590e1610804bc62f7d0
Gabriele <g.anteghini@campus.unimi.it>
2021-12-16 09:57:30 +0100

    modifica UserLogUtente

--- app/src/main/java/com/example/grandtour/ui/user/UserLogUtente.java
+++ app/src/main/java/com/example/grandtour/ui/user/UserLogUtente.java
@@ -1,15 +1,10 @@
 package com.example.grandtour.ui.user;
 
-import android.app.DatePickerDialog;
+
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,33 +13,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.grandtour.R;
-import com.example.grandtour.Utente;
-import com.example.grandtour.databinding.FragmentHomeBinding;
-import com.example.grandtour.databinding.FragmentUserSingupBinding;
-import com.example.grandtour.ui.home.HomeViewModel;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
+import com.example.grandtour.databinding.FragmentHomeBinding;
+import com.example.grandtour.ui.home.HomeViewModel;
 
 public class UserLogUtente extends Fragment {
 
-    private HomeViewModel UserViewModel;
+    private UserViewModel UserViewModel;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         UserViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+                new ViewModelProvider(this).get(UserViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
