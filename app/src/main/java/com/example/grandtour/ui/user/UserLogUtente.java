b875b5f8c4a8df33fa4c0cc72126a90cfd4e6898
Gabriele <g.anteghini@campus.unimi.it>
2021-12-24 12:17:17 +0100

    pagina log.

--- app/src/main/java/com/example/grandtour/ui/user/UserLogUtente.java
+++ app/src/main/java/com/example/grandtour/ui/user/UserLogUtente.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,17 +16,32 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 
+import com.example.grandtour.R;
 import com.example.grandtour.databinding.FragmentUserBinding;
 import com.example.grandtour.databinding.FragmentUserLogBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserLogUtente extends Fragment {
 
     private UserViewModel UserViewModel;
     private FragmentUserLogBinding binding;
 
+    private TextView profilo;
+    private TextView preferiti;
+    private TextView contatti;
+    private TextView recensione_app;
+    private Button button_profilo;
+    private Button button_preferiti;
+    private Button button_contatti;
+    private Button button_recensione_app;
+
+    private FirebaseAuth mAuth;
+    private final String TAG = "HOME";
+
+    final private String TAG_S = "USER";
+
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState)
-    {
+                             ViewGroup container, Bundle savedInstanceState) {
         UserViewModel = new ViewModelProvider(this).get(UserViewModel.class);
 
         binding = FragmentUserLogBinding.inflate(inflater, container, false);
@@ -37,6 +54,43 @@ public class UserLogUtente extends Fragment {
                 textView.setText(s);
             }
         });
+
+        profilo = root.findViewById(R.id.casella1);
+        preferiti = root.findViewById(R.id.casella2);
+        contatti = root.findViewById(R.id.casella3);
+        recensione_app = root.findViewById(R.id.casella4);
+
+        button_profilo = root.findViewById(R.id.casella1);
+        button_preferiti = root.findViewById(R.id.casella2);
+        button_contatti = root.findViewById(R.id.casella3);
+        button_recensione_app = root.findViewById(R.id.casella4);
+        //mAuth = FirebaseAuth.getInstance();
+
+
+        button_profilo.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                if (button_profilo.getVisibility() == View.GONE) {
+                    profilo.setVisibility(View.VISIBLE);
+                }
+                else
+                    profilo.setVisibility(View.GONE);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
         return root;
     }
 
